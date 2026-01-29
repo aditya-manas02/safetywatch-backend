2ceafb34218303993d8ffc95903fe4accd5a8aea
Aditya Manas <manasaditya7907@gmail.com>
2026-01-29 23:44:06 +0530

    Update backend

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,12 @@
-import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import pluginJs from "@eslint/js";
 
-export default tseslint.config(
-  { ignores: ["dist"] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
+    languageOptions: { globals: globals.node },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "@typescript-eslint/no-unused-vars": "off",
-    },
+      "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+    }
   },
-);
+  pluginJs.configs.recommended,
+];
