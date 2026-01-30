de4e003e800e0b953cd9453e158320177abd6f8c
Aditya Manas <manasaditya7907@gmail.com>
2026-01-30 18:25:39 +0530

    Enhance Profile: Add Picture Upload & GET /profile route

--- src/models/User.js
+++ src/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String },
   passwordHash: { type: String, required: true },
+  profilePicture: { type: String },
   createdAt: { type: Date, default: Date.now },
   isVerified: { type: Boolean, default: false },
   otp: { type: String },
