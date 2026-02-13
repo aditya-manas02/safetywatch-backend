1292f531a3b03ed38e767566b8849729e46fafb1
Aditya Manas <manasaditya7907@gmail.com>
2026-02-13 11:33:47 +0530

    fix: increase payload limit to resolve 413 error

--- src/server.js
+++ src/server.js
@@ -145,8 +145,8 @@ const limiter = rateLimit({
 app.use(limiter);
 
 /* ----------------------- BODY ----------------------- */
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static('public')); // Serve APK and other static assets
 
 /* ----------------------- LOGGING ------------------------ */
